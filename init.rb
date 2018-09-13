7387fec07ba7a8b956dda8305e78be851a7fa4da
Hydrant <zhangqiuyun@infohold.com.cn>
2018-09-13 11:48:25 +0800

    修改配置key

--- init.rb
+++ init.rb
@@ -2,12 +2,12 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_gitlab_hook do
   name 'Redmine GitLab Hook plugin'
-  author 'Phlegx Systems'
+  author 'zhangqiuyun@infohold.com.cn'
   description 'This plugin allows your Redmine installation to receive GitLab post-receive notifications'
   version '0.2.2'
-  url 'https://github.com/phlegx/redmine_gitlab_hook'
-  author_url 'https://github.com/phlegx'
+  url 'https://github.com/qbcbyb/redmine_gitlab_hook'
+  author_url 'https://github.com/qbcbyb'
   requires_redmine :version_or_higher => '2.3.0'
 
-  settings :default => { :all_branches => 'yes', :prune => 'yes', :auto_create => 'yes', :fetch_updates => 'yes' }, :partial => 'settings/gitlab_settings'
+  settings :default => { :all_branches => 'yes', :prune => 'yes', :auto_create => 'yes', :fetch_updates => 'yes' }, :partial => 'settings/gitlab_hook_settings'
 end
