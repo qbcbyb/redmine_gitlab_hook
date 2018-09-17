6fae88200772ed49f72fd18d861cbe799aab1249
Hydrant <zhangqiuyun@infohold.com.cn>
2018-09-17 09:15:28 +0800

    加入GIT用户名及密码的配置

--- init.rb
+++ init.rb
@@ -9,5 +9,12 @@ Redmine::Plugin.register :redmine_gitlab_hook do
   author_url 'https://github.com/qbcbyb'
   requires_redmine :version_or_higher => '2.3.0'
 
-  settings :default => { :all_branches => 'yes', :prune => 'yes', :auto_create => 'yes', :fetch_updates => 'yes' }, :partial => 'settings/gitlab_hook_settings'
+  settings :default => { 
+    :all_branches => 'yes', 
+    :prune => 'yes', 
+    :auto_create => 'yes', 
+    :fetch_updates => 'yes',
+    :git_user_name=> '',
+    :git_user_password=> ''
+  }, :partial => 'settings/gitlab_hook_settings'
 end
