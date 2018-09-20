411c1c78731e73727a7dbebb90e542f8a5fb9139
Hydrant <qbcbyb@sina.com>
2018-09-20 10:57:22 +0800

    修改TOKEN中value的长度限制为64

--- db/migrate/001_change_tokens_value_length_limit_to_64.rb
+++ db/migrate/001_change_tokens_value_length_limit_to_64.rb
@@ -1,8 +1,9 @@
-class ChangeTokensValueLimitTo64 < ActiveRecord::Migration::V4_2
+class ChangeTokensValueLimitTo64 < ActiveRecord::Migration[4.2]
   def up
     change_column :tokens, :value, :string, :limit => 64
   end
 
   def down
+    change_column :tokens, :value, :string, :limit => 40
   end
 end
