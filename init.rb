fa7eb167048046e50278c0e204278b97a287ccd0
Hydrant <qbcbyb@sina.com>
2018-09-20 09:55:30 +0800

    版本更新

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_gitlab_hook do
   name 'Redmine GitLab Hook plugin'
   author 'zhangqiuyun@infohold.com.cn'
   description 'This plugin allows your Redmine installation to receive GitLab post-receive notifications'
-  version '0.2.2'
+  version '1.0.0'
   url 'https://github.com/qbcbyb/redmine_gitlab_hook'
   author_url 'https://github.com/qbcbyb'
   requires_redmine :version_or_higher => '2.3.0'
